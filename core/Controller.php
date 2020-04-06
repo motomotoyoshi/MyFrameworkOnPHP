99a13fc2a942d11574a106399c07dd5ec8f6b586
motomotoyoshi <motomoto.git@gmail.com>
2020-04-06 21:38:54 +0900

    Update, Controller.php

--- core/Controller.php
+++ core/Controller.php
@@ -9,6 +9,7 @@ abstract class Controller
     protected $response;
     protected $session;
     protected $db_manager;
+    protected $auth_actions = array();
 
     public function __construct($application)
     {
@@ -31,9 +32,25 @@ abstract class Controller
             $this->forward404();
         }
 
+        if ($this->needsAuthentication($action) && !$this->session->isAuthenticated())
+        {
+            throw new UnauthorizedActionException();
+        }
+
         $content = $this->$action_method($params);
 
         return $content;
+    }
+
+
+    protected function needsAuthentication($action)
+    {
+        if ($this->auth_actions === true || (is_array($this->auth_actions) && in_array($action, $this->auth_actions))) {
+            return true;
+        }
+
+        return false;
+        
     }
 
 
